0286a399689d1e71a50f6c44f5f6a92c9463ff7e
Matthew Hyeun <jhyeun@uwaterloo.ca>
2023-03-08 17:57:52 -0500

    Add spring boot backend boilerplate, organize files

---
+++ gymstagram-backend/src/test/java/com/gymstagram/backend/GymstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymstagram.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
