4bde015f46beb78b1133d8f8cee6592b181e76b3
Danny Shin <d24shin@uwaterloo.ca>
2023-03-12 20:24:15 -0400

    new connection

--- android-app/app/src/main/java/com/example/gymstagram/NewWorkout.java
+++ android-app/app/src/main/java/com/example/gymstagram/NewWorkout.java
@@ -1,20 +1,39 @@
 package com.example.gymstagram;
 
 import android.os.Bundle;
-
+import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.gymstagram.databinding.ActivityMainBinding;
 import com.example.gymstagram.databinding.FragmentNewWorkoutBinding;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class NewWorkout extends Fragment {
 
     private FragmentNewWorkoutBinding binding;
+
+    TextInputEditText name, reps, sets, weight, duration;
+    Button add_workout;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+    }
 
     @Override
     public View onCreateView(
@@ -23,7 +42,10 @@ public class NewWorkout extends Fragment {
     ) {
 
         binding = FragmentNewWorkoutBinding.inflate(inflater, container, false);
+        View view = inflater.inflate(R.layout.fragment_new_workout, container, false);
+        TextView name = view.findViewById(R.id.name);
         return binding.getRoot();
+
 
     }
 
