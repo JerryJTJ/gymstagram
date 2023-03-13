63d6ab4ac905e37c59fed7ac15ae7884edf7f81a
Danny Shin <d24shin@uwaterloo.ca>
2023-03-12 21:08:09 -0400

    fix

--- android-app/app/src/main/java/com/example/gymstagram/WorkoutService.java
+++ android-app/app/src/main/java/com/example/gymstagram/WorkoutService.java
@@ -4,7 +4,7 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 public interface WorkoutService {
-@POST("/")
+@POST("http://10.0.2.2:8080/")
 Call<WorkoutResponse> saveWorkout(@Body WorkoutRequest workoutRequest);
 }
 
